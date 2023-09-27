b249bf79c829e6544257dc61d982f6ca04ab3c0d
Guyo <guyo.halake@students.moringaschool.com>
2023-09-27 12:08:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const initialState = 0; // Initialize the totalBatteries to 0
+
+// Reducer function
+function batteryReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_BATTERIES':
+      // Update the totalBatteries by adding the value of the new ones
+      return state + action.payload;
+    default:
+      return state;
+  }
+}
+
+// Your array of battery batches
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calculate the total number of batteries using the reducer
+const totalBatteries = batteryBatches.reduce((state, batch) => {
+  return batteryReducer(state, { type: 'ADD_BATTERIES', payload: batch });
+}, initialState);
+
+console.log(totalBatteries); // Output: 31
